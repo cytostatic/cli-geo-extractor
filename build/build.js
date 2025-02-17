a2ed44749a2858272efd2ed2ae13da90f4211a0e
Raphael Stärk <dj.rst@icloud.com>
2025-02-17 11:53:10 +0100

    Refactor build script for improved readability and enable verbose logging

--- build/build.js
+++ build/build.js
@@ -1,4 +1,4 @@
-const { compile } = require('nexe')
+const { compile } = require('nexe');
 const glob = require('glob');
 const fs = require('fs');
 
@@ -9,28 +9,29 @@ compile({
     name: fileName,
     build: true,
     ico: './build/pin-point.png',
-    verbose: false
-}).then(() => {
-    console.log('compile successfully!')
+    verbose: true,
+})
+    .then(() => {
+        console.log('compile successfully!');
 
-    return new Promise((resolve, reject) => {
-        glob('./geoext*', null, (error, files) => {
-            if (error) {
-                reject(error);
-                return;
-            }
+        return new Promise((resolve, reject) => {
+            glob('./geoext*', null, (error, files) => {
+                if (error) {
+                    reject(error);
+                    return;
+                }
 
-            resolve(files);
+                resolve(files);
+            });
         });
+    })
+    .then((files) => {
+        files.forEach((file) => {
+            const distPath = `./dist/${file}`;
+
+            fs.renameSync(file, distPath);
+        });
+    })
+    .catch((error) => {
+        console.error(error);
     });
-
-}).then((files) => {
-    files.forEach((file) => {
-        const distPath = `./dist/${file}`;
-
-        fs.renameSync(file, distPath);
-    });
-
-}).catch(error => {
-    console.error(error);
-});
